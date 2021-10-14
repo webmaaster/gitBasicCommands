4177ed7f6015908907a9e0fc3108d72404159e24
webmaaster <wecodewetest@gmail.com>
2021-10-14 19:04:03 -0400

    Basic Git Commands

--- GitBasicCommands.java
+++ GitBasicCommands.java
@@ -4,6 +4,28 @@ public class GitBasicCommands {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * Centralized version Control System
+
+Distributed Version Control System
+
+have git installed in your laptop
+create a folder and create the necessary java files(created a java project - UtilityProject using eclipse) - this acts as the working directory
+move into the java project folder (UtilityProject)
+create git local repository
+open git bash here
+run the command - git init
+this command will create a .git hidden folder inside UtilityProject folder
+move the files from working directory to the staging area
+run the command - git add .
+move files from staging area to local repository
+run the command - git commit -m "initial commit"
+next we create a remote repository(UtilityRepository) in out github account
+come back to the bit bash and set the remote url
+run the command - git remote add origin https://github.com/TeamLeadDemo/UtilityRepository.git
+push the code from local repository to the remote repository
+run the command - git push -u origin master
+		 */
 
 	}
 
